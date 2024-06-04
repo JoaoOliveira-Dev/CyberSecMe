140e046995a74f65fe8ae70cc0064108743a48b8
John Lennon <joaooli17@hotmail.com>
2024-06-04 10:29:04 -0300

    Adicionando nova rota

--- front-end/cybersecme/src/routes.js
+++ front-end/cybersecme/src/routes.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Home from "./App";
-import LoginPage from "./app/login";
+import LoginPage from "./app/pages/LoginPage";
+import CreateAccountPage from "./app/pages/CreateAccountPage";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<LoginPage />} path="/" />
+        <Route element={<CreateAccountPage />} path="/createaccount" />
       </Routes>
       {/* <Route component={LoginPage} path="/login" /> */}
     </BrowserRouter>
