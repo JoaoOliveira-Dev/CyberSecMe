78c58a2f055da27f2864e7bcdf313cb5153586fa
John Lennon <joaooli17@hotmail.com>
2024-06-04 09:47:00 -0300

    Controle de rotas front-end

--- front-end/cybersecme/src/routes.js
+++ front-end/cybersecme/src/routes.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Home from "./App";
 import LoginPage from "./app/login";
 
-const Routes = () => {
+const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Route component={LoginPage} path="/" exact />
+      <Routes>
+        <Route element={<LoginPage />} path="/" />
+      </Routes>
       {/* <Route component={LoginPage} path="/login" /> */}
     </BrowserRouter>
   );
 };
 
-export default Routes;
+export default AppRoutes;
