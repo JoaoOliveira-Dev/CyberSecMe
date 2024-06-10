e460979589326511db49ff32db2d8d0dd789b48b
John Lennon <joaooli17@hotmail.com>
2024-06-10 10:49:46 -0300

    rota para pegar usuários

---
+++ back-end/src/controllers/user.js
@@ -0,0 +1,15 @@
+const db = require("../db");
+
+const getUsers = (req, res) => {
+  const q = "SELECT * FROM tab_usuario";
+
+  db.query(q, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error fetching users");
+    }
+    return res.status(200).send(result);
+  });
+};
+
+module.exports = getUsers;
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const routes = express.Router();
+const getUsers = require("./controllers/user");
 
 routes.post("/login", (req, res) => {
   const { email, password } = req.body;
   res.send(`Email: ${email} / Senha: ${password}`);
 });
+
+routes.get("/getusers", getUsers);
 
 module.exports = routes;
